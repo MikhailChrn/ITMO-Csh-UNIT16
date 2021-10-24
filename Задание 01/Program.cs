49f8c25a28a25b3c81a13801f0ed738aa3cf5f68
Михаил <@HP-PAVILION>
2021-10-24 18:09:32 +0400

    Добавьте файлы проекта.

---
+++ Задание 01/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Text.Json.Serialization;
+using System.IO;
+
+namespace Задание_01
+{
+    //1.Необходимо разработать программу для записи информации о товаре в текстовый файл в формате json.
+    //Разработать класс для моделирования объекта «Товар».
+    //Предусмотреть члены класса «Код товара» (целое число), «Название товара» (строка), «Цена товара» (вещественное число).
+
+    //Создать массив из 5-ти товаров, значения должны вводиться пользователем с клавиатуры.
+    //Сериализовать массив в json-строку, сохранить ее программно в файл «Products.json».
+
+    class Product
+    {
+        [JsonPropertyName("productCode")]
+        public int ProductCode { get; set; }
+
+        [JsonPropertyName("productName")]
+        public string ProductName { get; set; }
+
+        [JsonPropertyName("productPrice")]
+        public double ProductPrice { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Данный блок активизирует работу с файлом с соответствующим адресом
+            string sourceFile = @"C:\Users\Михаил\Desktop\ИТМО\Блок 1\Занятие 16\Products.json";
+            StreamWriter sw = new StreamWriter(sourceFile, true);
+            //Параметр true для sreamwriter указывает на накапливание информации в текущем файле
+                                    
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            //Данный блок взят из видеоурока. Он подключает возможность работы с Cyrillic символами
+            //Также каждый новый атрибут JSON печатается с новой строки
+            {
+                Encoder = JavaScriptEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Cyrillic),
+                WriteIndented = false
+            };
+
+            const int n = 5; //Задаём количество товаров в массиве
+            Product[] product = new Product[n]; //Объявляем массив из экземпляров класса Product
+            string[] jsonStringArray = new string[n]; //Объявляем массив строк для работы JSON
+
+            
+            for (int i = 0; i < n; i++) //С использованием цикла последовательно заполняем все члены массива из класса Product вручную;
+            {
+                product[i] = new Product(); // Инициализирую каждый новый элемент массива класса Product, ИНАЧЕ появляется ошибка !!
+
+                product[i].ProductCode = i + 1;
+                Console.WriteLine("Введите нименование {0} товара:", product[i].ProductCode);
+                product[i].ProductName = Console.ReadLine();
+            ReadPrice:
+                Console.WriteLine("Введите цену данного товара: ");
+                try
+                {
+                    product[i].ProductPrice = Convert.ToDouble(Console.ReadLine());
+                }
+                catch { Console.WriteLine("Введено некорректное значение!"); goto ReadPrice; };
+
+                jsonStringArray[i] = JsonSerializer.Serialize(product[i], options); //Тут же сериализуем Product в массив из JSON строк
+
+            }
+            //Данный блок сохоаняет все строки в файл;
+            for (int i = 0; i < n; i++)
+            {
+                sw.WriteLine(jsonStringArray[i]);
+            }
+
+            sw.Close(); //Закрываем сеанс работы с файлом
+        }
+    }
+
+}
---
+++ Задание 02/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Text.Json.Serialization;
+using System.IO;
+
+namespace Задание_02
+
+//2.Необходимо разработать программу для получения информации о товаре из json-файла.
+//Десериализовать файл «Products.json» из задачи 1. Определить название самого дорогого товара.
+{
+    class Product //Класс для работы с товарами
+    {
+        [JsonPropertyName("productCode")]
+        public int ProductCode { get; set; }
+
+        [JsonPropertyName("productName")]
+        public string ProductName { get; set; }
+
+        [JsonPropertyName("productPrice")]
+        public double ProductPrice { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceFile = @"C:\Users\Михаил\Desktop\ИТМО\Блок 1\Занятие 16\Products.json";
+            StreamReader sr = new StreamReader(sourceFile); //Объявляем метод StreamReader
+            int stringCount = File.ReadAllLines(sourceFile).Length;//Считаем кол-во строк в файле;
+
+            string jsonString;
+            Product[] product = new Product[stringCount]; //Вынужден объявлять массив на конкретное кол-во экземпляров
+            int i = 0;
+
+            while ((jsonString = sr.ReadLine()) != null)
+            {
+                product[i] = new Product(); //Эта конструкция не работает, если при объявлении массива отсутсвует stringCount
+                product[i] = JsonSerializer.Deserialize<Product>(jsonString);
+                i++;
+            }
+
+            for (i = 0; i < stringCount; i++) //Данная конструкция выводит элементы всех экземпляров
+            {
+                Console.Write("{0,-5}  ", product[i].ProductCode);
+                Console.Write("{0,-30}  ", product[i].ProductName);
+                Console.WriteLine("{0:F} руб.", product[i].ProductPrice);
+            }
+            Console.ReadKey();
+        }
+    }
+}
